154753ab89a87f0dd4d028d608de015bbd033d8f
Teamofab <95554205+Teamofab@users.noreply.github.com>
2022-12-13 10:49:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Teamofab/AllXUrlXUploader /LazyDeveloper
+git clone https://github.com/Teamofab/Xallxurluploader /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
